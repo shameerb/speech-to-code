cfe843db27b53749880caf7915871f02055c8aea
Shameer <shameer789@gmail.com>
2025-01-25 03:03:49 +0530

    reverted to openai

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ def main():
     config = load_config()
     speech_to_text = GoogleSpeechToText()
     # speech_to_text = WhisperSpeechToText()
-    # code_generation = OpenAICodeGeneration(config["openai_key"])
+    code_generation = OpenAICodeGeneration(config["openai_key"])
     # code_generation = DeepSeekCodeGeneration(config["deepseek_key"] )
     # code_generation = ClaudeCodeGeneration(config["anthropic_key"])
-    code_generation = TransformersCodeGeneration()
+    # code_generation = TransformersCodeGeneration()
     copilot = AudioCopilot(speech_to_text, code_generation)
     copilot.run()
 
